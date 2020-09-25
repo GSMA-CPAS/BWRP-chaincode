80d53ebb5e0b1c0881729fbcb3cb6aaf8849dcd6
Simon Schulz <simon.schulz01@telekom.de>
2020-09-25 16:56:05 +0200

    fix counterfeither dependency issue

--- hybrid/test/stubs/counterfeit.go
+++ hybrid/test/stubs/counterfeit.go
@@ -1,10 +1,8 @@
-package chaincode
+package stubs
 
 import (
-	"github.com/golang/protobuf/proto"
 	"github.com/hyperledger/fabric-chaincode-go/shim"
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
-	"github.com/hyperledger/fabric-protos-go/msp"
 )
 
 //go:generate counterfeiter -o ../mocks/chaincodestub.go -fake-name ChaincodeStub . chaincodeStub
@@ -15,10 +13,4 @@ type chaincodeStub interface {
 //go:generate counterfeiter -o ../mocks/transaction.go -fake-name TransactionContext . transactionContext
 type transactionContext interface {
 	contractapi.TransactionContextInterface
-}
-
-func createIdentity(mspID string, idbytes []byte) ([]byte, error) {
-	sid := &msp.SerializedIdentity{Mspid: mspID, IdBytes: idbytes}
-	b, err := proto.Marshal(sid)
-	return b, err
 }
