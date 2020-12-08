87f9783481f6aa0d5d829e4307fd013453c96303
dhruvagarwal86 <dhruv.agarwal.lnm@gmail.com>
2020-12-08 21:57:32 +0530

    Adding updated document.go

--- hybrid/test/data/document.go
+++ hybrid/test/data/document.go
@@ -9,15 +9,17 @@ import (
 // Document structure
 type Document struct {
 	Data64 string
+	Tmp    []byte
 	Hash   string
 }
 
-var data = []byte("data1234")
-var data64 = base64.StdEncoding.EncodeToString(data)
+var data = "data1234"
+var data64 = base64.StdEncoding.EncodeToString([]byte(data))
 var tmp = sha256.Sum256([]byte(data64))
 
 // ExampleDocument : a test document:
 var ExampleDocument = Document{
 	Data64: data64,
+	Tmp:    tmp[:],
 	Hash:   hex.EncodeToString(tmp[:]),
 }
